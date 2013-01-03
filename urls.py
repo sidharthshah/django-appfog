aab48adf0a0c9f98a66b6afe1f46b881641dbd00
Sidharth Shah <sidharth@fafadiatech.com>
2013-01-03 12:27:16 +0530

    fixing static files issues

--- urls.py
+++ urls.py
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
